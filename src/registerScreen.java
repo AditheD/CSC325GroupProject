9125f32ae6702d478ba7f28271caff0ec7228ec4
AditheD <adithedas20@gmail.com>
2025-07-19 15:39:10 -0400

    Initial commit  register screen

--- src/registerScreen.java
+++ src/registerScreen.java
@@ -1,19 +1,19 @@
-import com.google.type.PhoneNumber;
-import com.google.type.PhoneNumberOrBuilder;
-import javafx.concurrent.Task;
+package src;
+
 import javafx.scene.Scene;
-import javafx.scene.control.Label;
-import javafx.scene.layout.*;
-import javafx.stage.Stage;
 import javafx.scene.control.*;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import resources.FirebaseAuthService;
+import src.RegisterUser; // Assuming this is your user registration handler
+import src.DietAppMainScreen; // Assuming this is your main app screen
 
-
-
-public class registerScreen {
+public class RegisterScreen {
     private final Stage stage;
     private final FirebaseAuthService authService = new FirebaseAuthService();
 
-    public registerScreen(Stage stage) {
+    public RegisterScreen(Stage stage) {
         this.stage = stage;
     }
 
@@ -24,38 +24,42 @@ public class registerScreen {
 
         VBox regInfo = new VBox();
         regInfo.setId("regInfo");
+
         TextField username = new TextField();
         username.setPromptText("Enter Username");
-        TextField password = new PasswordField();
+
+        PasswordField password = new PasswordField();
         password.setPromptText("Enter Password");
+
         TextField email = new TextField();
         email.setPromptText("Enter Email");
+
         TextField phone = new TextField();
         phone.setPromptText("Enter Phone Number");
 
         Button submit = new Button("Submit");
         submit.setId("submitButton");
         submit.setOnAction(e -> {
-            registerUser registerUser = new registerUser();
             String emailString = email.getText();
             String phoneString = phone.getText();
             String usernameString = username.getText();
             String passwordString = password.getText();
 
-            registerUser.registerUser(emailString, phoneString, passwordString, usernameString);
+            RegisterUser registerUser = new RegisterUser();
+            registerUser.register(emailString, phoneString, passwordString, usernameString);
 
-            DietAppMainScreen dietAppMainScreen = new DietAppMainScreen(stage);
+            DietAppMainScreen dietAppMainScreen = new DietAppMainScreen();
             dietAppMainScreen.show();
-
         });
 
-        regInfo.getChildren().addAll(username, password, email, phone, title,submit);
-        root.setCenter(regInfo);
+        regInfo.getChildren().addAll(username, password, email, phone, submit);
+
         root.setTop(title);
+        root.setCenter(regInfo);
+
         Scene scene = new Scene(root, 700, 600);
         stage.setScene(scene);
         stage.setTitle("Registration Screen");
         stage.show();
-
     }
 }
